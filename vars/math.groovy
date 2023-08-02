04c887a22c4d79d0b92786bd55c3391bd5c57c16
321ckmdevops <140068916+321ckmdevops@users.noreply.github.com>
2023-08-02 18:04:53 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,17 @@
+def sum(a,b)
+{
+  ecbho "The sum of ${a} and ${b} is ${a+b}"
+}
+def sub(a,b)
+{
+  ecbho "The substraction of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  ecbho "The multipcation of ${a} and ${b} is ${a*b}"
+}
 
+def dev(a,b)
+{
+  ecbho "The devision of ${a} and ${b} is ${a/b}"
+}
